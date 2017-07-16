022dd9df2ca9519432f0787676920b51492fcf0f
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-07-16 12:10:51 +0530

    Define pattern functors for lists, trees and naturals

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,47 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Main where
 
+import Data.Functor.Foldable hiding (ListF)
+import Data.List.Ordered(merge)
+import Prelude hiding (Foldable, succ)
+
 import Lib
+
+data Natural =
+    Zero
+  | Succ Natural
+
+data NatF r =
+    ZeroF
+  | SuccF r
+  deriving (Show, Functor)
+
+data ListF a r =
+    NilF
+  | ConsF a r
+  deriving (Show, Functor)
+
+data TreeF a r =
+    EmptyF
+  | LeafF a
+  | NodeF r r
+  deriving (Show, Functor)
+
+type Nat = Fix NatF
+type List a = Fix (ListF a)
+type Tree a = Fix (TreeF a)
+
+zero :: Nat
+zero = Fix ZeroF
+
+succ :: Nat -> Nat
+succ = Fix . SuccF
+
+nil :: List a
+nil = Fix NilF
+
+cons :: a -> List a -> List a
+cons x xs = Fix (ConsF x xs)
 
 main :: IO ()
 main = someFunc
